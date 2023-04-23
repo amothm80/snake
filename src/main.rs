f05d30d84e240d3b853d63ac23fe86fa3ddd4348
Ahmed Aluthman <amo.1980@gmail.com>
2023-04-23 20:43:50 +0300

    v0.2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-//use rand;
-//use piston_window;
+use rand;
+use piston_window;
 mod draw;
 mod snake;
 mod game;
+
+use piston_window::*;
+use piston_window::types::Color;
+
+use game::Game;
+use draw::to_coord_u32;
+
+const BACK_COLOR: Color = [0.5,0.5,0.5,1.0];
 fn main() {
-    let mut d:snake::Direction = snake::Direction::Up;
-    d.opposite();
-    println!("Hello, world!");
+    let (width,height) = (30,30);
+
+    let mut window: PistonWindow = WindowSettings::new(
+        "Snake",
+        [to_coord_u32(width), to_coord_u32(height)],
+    ).exit_on_esc(true).build().unwrap();
 }
